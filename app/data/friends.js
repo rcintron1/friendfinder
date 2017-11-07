c0ea869fe4390b3c989f7db18527b466627ad2f3
Rolando Cintron <rcintron@ndi.org>
2017-11-07 17:44:56 -0500

    Adding api data

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,6 +1,7 @@
 var friendData = [
     {
         name: "Roli",
+        email: "rolo@polo.com",
         answers: {1:4,2:2,3:3,4:3,5:3,6:4,7:4,8:5,9:4,10:3}
     }
 ];
--- server.js
+++ server.js
@@ -1,8 +1,11 @@
-var express = require("express"), parser = require("body-parser"), PORT = 8000;
-
+var express = require("express"), bodyParser = require("body-parser"), PORT = 8000;
 
 var app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
 
 app.listen(PORT, function() {
