5724a1ab88b2205dbfa1b1a1772533284e03fbdb
Rolando Cintron <rolando.cintron@gmail.com>
2017-11-09 23:45:52 -0500

    Adding logic to select closest candidate

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,7 +2,7 @@ var friendData = [
     {
         name: "Roli",
         photo: "https://scontent-iad3-1.xx.fbcdn.net/v/t1.0-1/p720x720/19642417_10155284979736346_6420438756396415525_n.jpg?oh=d74a8c065aa42bfdcb6d6c74671c5de5&oe=5A6ED51E",
-        scores: [4,2,3,3,3,4,4,5,4,3]
+        scores: [1,1,1,1,1]
     }
 ];
 
