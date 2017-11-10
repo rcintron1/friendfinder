81d20a685f99636cf92e9e3a25e4cce533f2e851
Rolando Cintron <rolando.cintron@gmail.com>
2017-11-10 13:26:13 -0500

    Finished Logic with selection and modal on Home page

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,7 +2,7 @@ var friendData = [
     {
         name: "Roli",
         photo: "https://scontent-iad3-1.xx.fbcdn.net/v/t1.0-1/p720x720/19642417_10155284979736346_6420438756396415525_n.jpg?oh=d74a8c065aa42bfdcb6d6c74671c5de5&oe=5A6ED51E",
-        scores: [1,1,1,1,1]
+        scores: [1,1,1,1,1,1,1,1,1,1]
     }
 ];
 
--- app/data/questions.js
+++ app/data/questions.js
@@ -3,7 +3,12 @@ var questions = [
     "I consider yoga a sport <-> If your bones have not been broken, you have not participated in a sport",
     "Fuchsia is the new Flavescent <-> I only know black & blue",
     "My mood is ambivolent at times <-> I don't know what that word is, but it's making me angry",
-    "Silence is the best protest <-> He/she didn't get the point until I hear 'Ow!'"
+    "Silence is the best protest <-> He/she didn't get the point until I hear 'Ow!'",
+    "I only work on jobs that meet my ethical values <-> Everyone's got a price, mine is cheap",
+    "We should not buy products where there is a potential of exploitation <-> If it's on sale, it's in my shopping cart",
+    "If I find some money, I'll do my best to find it's owner <-> If I find money, my networth just went up",
+    "Political values are to be shared and discussed <-> I vote for NonYaBusiness",
+    "I expect your help when ever necessary <-> I expect you will solve your issues and leave me out of it"
   ];
 
 module.exports = questions;
