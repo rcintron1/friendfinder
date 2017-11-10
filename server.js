e300d6137eac47f24123284ad429e24aa5a3ca18
Rolando Cintron <rolando.cintron@gmail.com>
2017-11-10 15:19:52 -0500

    fixed port issue

--- server.js
+++ server.js
@@ -8,6 +8,6 @@ app.use(bodyParser.json());
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
 
-app.listen(PORT, function() {
+app.listen(process.env.PORT || 5000, function() {
     console.log("App listening on PORT: " + PORT);
   });
